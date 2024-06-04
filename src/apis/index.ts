a4b332772907fd75bdaae34fb9a8da99d6651144
Chixol <phw9278@gmail.com>
2024-06-04 16:47:13 +0900

    test

---
+++ src/apis/index.ts
@@ -0,0 +1,18 @@
+import { AxiosResponse } from "axios";
+import ResponseDto from "./response.dto";
+
+// function : Request 처리 함수
+export const requestHandler = <T>(response: AxiosResponse<T, any>) => {
+    const responseBody = response.data as T;
+    return responseBody;
+};
+
+// function : Request Error 처리 함수
+export const requestErrorHandler = (error: any) => {
+    const responseBody = error.response?.data;
+    if (!responseBody) return null; // undefined로 확인되지만 null로 바꾼것
+    return responseBody as ResponseDto;
+};
+
+// function: Authorization Bearer 헤더
+export const bearerAuthorization = (accessToken: string) => ({ headers: { 'Authorization': `Bearer ${accessToken}` } });
---
+++ src/components/Inputbox/index.tsx
@@ -0,0 +1,44 @@
+//  IdInputBox, PasswordInputBox 함수형 컴포넌트 생성
+// 각각의 컴포넌트는 모두 export 가능하도록 내보냄
+import React, { ChangeEvent, KeyboardEvent } from "react";
+import './style.css';
+
+export interface InputBoxProps {
+    label: string;
+    type: 'text' | 'password';
+    value: string;
+    placeholder: string;
+    onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+    buttonTitle?: string;
+    buttonStatus?: boolean;
+    onButtonClickHandler?: () => void;
+    message?: string;
+    error?: boolean;
+    onKeydownHandler? : (event: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle, buttonStatus, onButtonClickHandler, message, error, onKeydownHandler}: InputBoxProps) {
+
+    const buttonClass = buttonStatus ? 'input-primary-button' : 'input-disable-button';
+    const messageClass = 'input-message ' + (error ? 'error' : 'primary'); 
+
+    return (
+        <div className="input-box">
+            <div className="input-label label">{label}</div>
+            <div className="input-content-box">
+                <input 
+                    className="input" 
+                    type={type} 
+                    value={value} 
+                    placeholder={placeholder} 
+                    onChange={onChangeHandler} 
+                    onKeyDown={onKeydownHandler} />
+                { buttonTitle &&
+                <div className={buttonClass} onClick={onButtonClickHandler}>{buttonTitle}</div>}
+            </div>
+        <div className={messageClass}>
+            {message}
+        </div>
+    </div>
+    )
+}
